4de361604047e5398355ddc04ec66bc940eaaffe
Pratik Shrestha <shrepr0@furman.edu>
2025-09-01 20:38:48 -0500

    Fix: Remove basePath for github.io repository

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/github.io-1' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/github.io-1' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 export default nextConfig
